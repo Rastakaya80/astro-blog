9dcf09283ced3414dfceaf16006a8aa47d89907b
Rastakaya80 <rastakaya80@gmail.com>
2024-01-21 10:56:55 -0700

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Our Dental Blog';
+export const SITE_DESCRIPTION = 'Articles & Tips To Keep You Better Informed';
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
