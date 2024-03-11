499d0bb036917058750b0848d64411b63df8bf0e
Rastakaya80 <rastakaya80@gmail.com>
2024-03-11 16:27:21 -0700

    Added Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output:'server',// default is static. You can use hybrid to split parts between static and SSR
+  output: 'server' // default is static. You can use hybrid to split parts between static and SSR
+  ,
+  adapter: vercel()
 });
