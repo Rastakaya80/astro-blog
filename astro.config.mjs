7c3b64acf3c656bb9a14219578f114f8c5c20757
Rastakaya80 <rastakaya80@gmail.com>
2024-03-10 20:30:17 -0700

    finished project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output:'server',// default is static. You can use hybrid to split parts between static and SSR
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,32 @@
-export const SITE_TITLE = 'Our Dental Blog';
-export const SITE_DESCRIPTION = 'Articles & Tips To Keep You Better Informed';
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const SITE_TITLE = 'Our Blog';
+export const SITE_DESCRIPTION = 'Articles & Tips To Keep You Informed';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
+// export const LANGUAGES = ['en'];
+// export const DEFAULT_LANGUAGE = 'en';
+// export const COOKIE_CONSENT_ENABLED = true;
+export const BUSINESS_NAME =  'Tech People';
+export const LEGAL_NAME = 'Tech People Inc.';
+export const PHONE = '(123) 456-7890';
+// export const ADDRESS = '123 ABC Street, City, State ZIP';
+export const EMAIL = 'info@techpeople.com';
+export const ADDRESS =   {
+    street: "12345 El Camino Real",
+    city: "Los Angeles",  
+    state: "CA",    
+    zipcode: "90028"
+};
+export const SOCIAL_LINKS = {
+  facebook_url: "https://www.facebook.com/randy.stone.756",
+  // twitter: "http://twitter.com/",
+  // github: "http://github.com/",
+  // linkedin: "http://linkedin.com/",
+  // instagram: "http://instagram.com/"
+}
+// export const SOCIAL_LINKS = [
+    // { iconClass: "fa fa-twitter", url: "#" },
+    // { iconClass: "fa fa-facebook", url: "https://www.facebook.com/randy.stone.756" },
+    // { iconClass: "fa fa-google-plus", url: "https://plus.google.com/+JasonLian" }
+// ];
+// The key is used to check if a user has already authenticated with the provider, and the value (array) contains the scopes that
+// export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;   
